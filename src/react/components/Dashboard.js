15a7ca32c764a9f91499b8f990bc4059fa4b9688
Joel Denning <joeldenning@gmail.com>
2016-11-10 18:51:34 -0700

    Updates for surge

--- src/react/components/Dashboard.js
+++ src/react/components/Dashboard.js
@@ -10,8 +10,8 @@ class Dashboard extends Component {
         <h2>React + react-router</h2>
         <p>
           This is the react-router <a href="https://github.com/ReactTraining/react-router/tree/master/examples/huge-apps" target="_blank">huge-apps example</a>,
-          converted into a single-spa application. Note that the entire application is lazy loaded (single-spa's doing)
-		  and that each of the individual react-router routes is lazy loaded (react-router's doing).
+          converted into a single-spa application. Note that the entire application is lazy loaded (single-spa's does this)
+		  and that each of the individual react-router routes is lazy loaded (react-router's does this).
         </p>
         <h2>Courses</h2>{' '}
         <ul>
