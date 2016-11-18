032a316ba2fd87f38cd9c3285632a609a6452cbb
Justin McMurdie <mcmurdiej@gmail.com>
2016-11-18 15:26:46 -0700

    Enabling production mode in angular 2. This disables a bunch of internal angular 2 checks and prevents a console warning. (#15)

--- src/angular2/main-module.ts
+++ src/angular2/main-module.ts
@@ -1,6 +1,9 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {ExampleNg2App} from './example-ng2-app.component.ts';
+import {enableProdMode} from '@angular/core';
+
+enableProdMode()
 
 @NgModule({
   imports: [BrowserModule],
