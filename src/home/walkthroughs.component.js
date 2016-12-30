bab1adcfa6b9ee15ea8632b5b4f45d4d482bd592
Joel Denning <joeldenning@gmail.com>
2016-12-29 21:52:38 -0700

    Adding explanation of multi-framework pages

--- src/home/walkthroughs.component.js
+++ src/home/walkthroughs.component.js
@@ -55,8 +55,35 @@ export default class Walkthroughs extends React.Component {
 						</div>
 					}
 				</li>
-				<li className={`collection-item ${css(styles.item)}`} onClick={() => alert("Example coming soon!")}>
+				<li className={`collection-item ${css(styles.item)}`} onClick={() => this.setState({third: !this.state.third})}>
 					Parts of the page written in one framework, while other parts use a different framework.
+					{this.state.third &&
+						<div>
+							<ol>
+								<li className={css(styles.explanation)}>
+									1. Click on the Framework Inspector in the navbar, to highlight which parts of the app are written in which framework
+								</li>
+								<li className={css(styles.explanation)}>
+									2. Inspect element on the navbar. Notice the &nbsp;<img src="/images/data-reactroot.png" />
+								</li>
+								<li className={css(styles.explanation)}>
+									3. Go to the Angular 1 app and inspect element. &nbsp;<img src="/images/ng-scope.png" />
+								</li>
+								<li className={css(styles.explanation)}>
+									This shows a simple example of having a part of page written in one framework (React) while another part
+									is written in a different framework (Angular). As you navigate through all the other apps in this demo,
+									you will be able to see many different frameworks all coexisting peacefully with the navbar.
+								</li>
+								<li className={css(styles.explanation)}>
+									<i>
+										The single-spa ideology is to have independent, self-contained apps make up an entire page. Instead of betting on
+										a single framework for a long time and for every feature, single-spa helps you adopt new frameworks as they are
+										invented.
+									</i>
+								</li>
+							</ol>
+						</div>
+					}
 				</li>
 			</ul>
 		);
