b508dbec5a7309af70a749cdbb03b0914c4947c8
Deewendra Shrestha <deewendra.shrestha@gmail.com>
2017-08-04 11:50:29 -0400

    WIP (#39)
    * WIP
    
    * get build folder name same as app name

--- src/ember-app/ember-cli-build.js
+++ src/ember-app/ember-cli-build.js
@@ -5,7 +5,8 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-		autoRun: false,
+    autoRun: false,
+    storeConfigInMeta: false
     // Add options here
   });
 
