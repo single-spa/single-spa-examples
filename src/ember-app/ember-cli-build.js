2031de39a5f0fe32682cda645faf5b73c600c19e
Joel Denning <joel.denning@canopytax.com>
2017-08-04 10:23:45 -0600

    Production build for ember app

--- src/ember-app/ember-cli-build.js
+++ src/ember-app/ember-cli-build.js
@@ -6,7 +6,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     autoRun: false,
-    storeConfigInMeta: false
+    storeConfigInMeta: false,
+		fingerprint: {
+			customHash: null,
+		},
     // Add options here
   });
 
