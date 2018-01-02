85032967c957defac9b3b8b536d4383ed157aa87
Rodrigo Erades Alonso <reradesa@everis.com>
2018-01-02 10:54:55 +0100

    Adding error handling for enableProdMode. If we use more than one Angular App that tries to enable prod mode, this will lead us to an Uncaught Error

--- src/angular2/main-module.ts
+++ src/angular2/main-module.ts
@@ -3,12 +3,16 @@ import {BrowserModule} from '@angular/platform-browser';
 import {ExampleNg2App} from './example-ng2-app.component.ts';
 import {enableProdMode} from '@angular/core';
 
-enableProdMode()
+try {
+    enableProdMode();
+} catch (err) {
+    console.info('EnableProdMode already set')
+}
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [ExampleNg2App],
-  bootstrap: [ExampleNg2App]
+    imports: [BrowserModule],
+    declarations: [ExampleNg2App],
+    bootstrap: [ExampleNg2App]
 })
 export default class MainModule {
 }
