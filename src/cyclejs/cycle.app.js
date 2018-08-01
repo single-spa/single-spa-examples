47174b562b17841e2991285b538c25fbd68ac17d
Landon Young <landon.young@canopytax.com>
2018-07-31 18:23:19 -0600

    simplify the app js file

--- src/cyclejs/cycle.app.js
+++ src/cyclejs/cycle.app.js
@@ -1,4 +1,3 @@
-// most of this is copied from the good work of our dear friend @pcmnac
 import {run} from '@cycle/run'
 import singleSpaCycle from '@pcmnac/single-spa-cycle';
 import App, {driversWithDomNode} from './cycle.root.js';
@@ -6,19 +5,10 @@ import App, {driversWithDomNode} from './cycle.root.js';
 const cycleLifecycles = singleSpaCycle({
   run,
   rootComponent: App,
-  drivers: driversWithDomNode(domElementGetter())
+  drivers: driversWithDomNode(
+    document.getElementById('cycle-app')
+  )
 });
-
-function domElementGetter() {
-  // Make sure there is a div for us to render into
-  let el = document.getElementById('cycle-app');
-  if (!el) {
-    el = document.createElement('div');
-    el.id = 'cycle-app';
-    document.body.appendChild(el);
-  }
-  return el;
-}
 
 export const bootstrap = [
   cycleLifecycles.bootstrap
