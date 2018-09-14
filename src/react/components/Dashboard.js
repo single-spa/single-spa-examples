924f46f977d7fc5b0c0478607f6e0fbb7d4822df
Josh Alling <joshralling@gmail.com>
2018-09-14 08:42:32 -0600

    fix broken huge-apps link (#59)

--- src/react/components/Dashboard.js
+++ src/react/components/Dashboard.js
@@ -9,7 +9,7 @@ class Dashboard extends Component {
       <div>
         <h2>React + react-router</h2>
         <p>
-          This is the react-router <a href="https://github.com/ReactTraining/react-router/tree/master/examples/huge-apps" target="_blank">huge-apps example</a>,
+          This is the react-router <a href="https://github.com/ReactTraining/react-router/tree/1.0.x/examples/huge-apps" target="_blank">huge-apps example</a>,
           converted into a single-spa application. Note that the entire application is lazy loaded (single-spa's does this)
       and that each of the individual react-router routes is lazy loaded (react-router's does this).
         </p>
